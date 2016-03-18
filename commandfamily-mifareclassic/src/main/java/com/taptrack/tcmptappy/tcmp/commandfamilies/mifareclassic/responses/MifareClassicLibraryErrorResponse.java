a817de9af2e125da0e92f0602b37ad52d50158c3
Luke Van Oort <johnvanoort@gmail.com>
2016-03-18 16:07:07 -0400

    added error codes to the mifare classic error response

--- commandfamily-mifareclassic/src/main/java/com/taptrack/tcmptappy/tcmp/commandfamilies/mifareclassic/responses/MifareClassicLibraryErrorResponse.java
+++ commandfamily-mifareclassic/src/main/java/com/taptrack/tcmptappy/tcmp/commandfamilies/mifareclassic/responses/MifareClassicLibraryErrorResponse.java
@@ -9,6 +9,48 @@ public class MifareClassicLibraryErrorResponse extends AbstractMifareClassicMess
     public static final byte COMMAND_CODE = 0x7F;
     protected StandardErrorResponseDelegate delegate;
 
+    public interface ErrorCodes {
+        /**
+         * A command parameter was not valid
+         */
+        public static final byte INVALID_PARAMETER = 0x01;
+        /**
+         * A command was received with an insufficient number
+         * of parameters
+         */
+        public static final byte TOO_FEW_PARAMETERS = 0x02;
+        /**
+         * Too many parameters were specified for command
+         */
+        public static final byte TOO_MANY_PARAMETERS = 0x03;
+        /**
+         * A polling error occured. This indicates a rare error mode from
+         * the reader itself.
+         */
+        public static final byte POLLING_ERROR = 0x04;
+        /**
+         * An error occured while attempting to read the tag
+         */
+        public static final byte TAG_READ_ERROR = 0x05;
+        /**
+         * The start block is after the end block
+         */
+        public static final byte INVALID_BLOCK_ORDER = 0x06;
+        /**
+         * There was an error attempting to authenticate with the tag
+         */
+        public static final byte AUTHENTICATION_ERROR = 0x07;
+        /**
+         * The block number specified is not valid. This occurs when
+         * the user specifies a block over 0x3F on a 1K tag
+         */
+        public static final byte INVALID_BLOCK_NO = 0x08;
+        /**
+         * The key number specified is not valid
+         */
+        public static final byte INVALID_KEY_NO = 0x09;
+    }
+
     public MifareClassicLibraryErrorResponse() {
         delegate = new StandardErrorResponseDelegate();
     }
